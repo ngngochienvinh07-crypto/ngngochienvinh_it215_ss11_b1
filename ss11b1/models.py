1685a5b54ec0a1e3975384066443ff250034012d
ngngochienvinh07-crypto <ngngochienvinh07@gmail.com>
2026-07-09 15:08:00 +0700

    Update models.py

--- ss11b1/models.py
+++ ss11b1/models.py
@@ -1,12 +1,11 @@
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean
-from database import Base
+Base = declarative_base()
 
-
-class ParkingSlot(Base):
+class ParkingSlotModel(Base):
     __tablename__ = "parking_slots"
-
-    id = Column(Integer, primary_key=True, index=True)
-    slot_code = Column(String(50), unique=True, nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    slot_code = Column(String(50), nullable=False)
     zone_name = Column(String(255), nullable=False)
     max_weight = Column(Integer, nullable=False)
     is_available = Column(Boolean, default=True)
