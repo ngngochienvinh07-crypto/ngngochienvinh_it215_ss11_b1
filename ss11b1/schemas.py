985fe2ae3accc13819fd8f62e877aa132f3d53cb
ngngochienvinh07-crypto <ngngochienvinh07@gmail.com>
2026-07-09 15:08:14 +0700

    Update schemas.py

--- ss11b1/schemas.py
+++ ss11b1/schemas.py
@@ -1,2 +1,7 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+from pydantic import BaseModel
+
+class CreateParkingSlot(BaseModel):
+    slot_code: str
+    zone_name: str
+    max_weight: int
+    is_available: bool = True
