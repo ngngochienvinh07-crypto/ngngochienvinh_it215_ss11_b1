9b56bc647f86a10f55bad0ab156a017243773ace
ngngochienvinh07-crypto <ngngochienvinh07@gmail.com>
2026-07-09 15:06:28 +0700

    Update service.py

--- ss11b1/service.py
+++ ss11b1/service.py
@@ -1,2 +1,18 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+from sqlalchemy.orm import Session
+from models import ParkingSlotModel
+from schemas import CreateParkingSlot
+
+def get_all_parking_slots_service(db: Session):
+    list_slots = db.query(ParkingSlotModel).all()
+    return list_slots
+
+def get_parking_slot_by_id_service(id: int, db: Session):
+    slot_in_db = db.query(ParkingSlotModel).filter(ParkingSlotModel.id == id).first()
+    return slot_in_db
+
+def create_parking_slot_service(new_slot: CreateParkingSlot, db: Session):
+    slot = ParkingSlotModel(**new_slot.model_dump())
+    db.add(slot)
+    db.commit()
+    db.refresh(slot)
+    return slot
